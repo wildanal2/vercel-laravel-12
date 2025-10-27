3b71e31e8f2c79c19e933823884e3be4f5c07c32
wildanal2 <wildanal2@gmail.com>
2025-10-27 19:08:14 +0700

    Refactor asset routing in vercel.json and remove base path from vite.config.js for improved deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
